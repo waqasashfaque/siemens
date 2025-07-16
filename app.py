bcafa0b816d093205c6cf8613ef0479252b25f28
waqasashfaque <153775325+waqasashfaque@users.noreply.github.com>
2025-07-17 00:03:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def process_kobo_data(data1, data2):
     merged['MONTH'] = merged['Complaint_Reg_Date'].dt.strftime('%B')
     # Set MONTH as categorical with calendar order
     month_order = [
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        'January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'
     ]
     merged['MONTH'] = pd.Categorical(merged['MONTH'], categories=month_order, ordered=True)
     merged['Year'] = merged['Complaint_Reg_Date'].dt.year
